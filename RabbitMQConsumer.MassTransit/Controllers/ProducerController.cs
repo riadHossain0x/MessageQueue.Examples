7273f04904e42ecc2775f2aa293c027258cc1d6e
Riad <riadofficial.me@gmail.com>
2024-04-23 11:27:04 +0600

    rabbitmq specific implementation

---
+++ RabbitMQConsumer.MassTransit/Controllers/ProducerController.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using MassTransit.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQConsumer.MassTransit.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProducerController : ControllerBase
+{
+    private readonly IBus _bus;
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public ProducerController(IBus bus, IPublishEndpoint publishEndpoint)
+    {
+        _bus = bus;
+        _publishEndpoint = publishEndpoint;
+    }
+
+    [HttpPost]
+    [Route("Create")]
+    public async Task<IActionResult> Create([FromBody] Order order)
+    {
+        //await _publishEndpoint.Publish<Order>(order);
+        var endpoint = await _bus.GetSendEndpoint(new Uri("exchange:order-exchange"));
+        await endpoint.Send(order);
+        return Ok(order);
+    }
+}
---
+++ RabbitMQConsumer.MassTransit/Program.cs
@@ -0,0 +1,67 @@
+using MassTransit;
+using MassTransit.Models;
+using RabbitMQ.Client;
+using RabbitMQConsumer.MassTransit.Consumers;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host("amqp://guest:guest@localhost:5672");
+        cfg.ConfigureEndpoints(ctx);
+
+        cfg.Message<Order>(x => x.SetEntityName("order-exchange"));
+        cfg.Publish<Order>(x => x.ExchangeType = ExchangeType.Fanout);
+
+        cfg.ReceiveEndpoint($"order-queue-{Guid.NewGuid()}", x =>
+        {
+            x.ConfigureConsumeTopology = false;
+            x.AutoDelete = true;
+            x.Consumer<OrderConsumer>();
+            x.Bind("order-exchange", s =>
+            {
+                s.ExchangeType = ExchangeType.Fanout;
+                //s.RoutingKey = "order-key";
+            });
+        });
+
+        cfg.ReceiveEndpoint($"order-queue-{Guid.NewGuid()}", x =>
+        {
+            x.ConfigureConsumeTopology = false;
+            x.AutoDelete = true;
+            x.Consumer<OrderConsumer2>();
+            x.Bind("order-exchange", s =>
+            {
+                s.ExchangeType = ExchangeType.Fanout;
+                //s.RoutingKey = "order-key";
+            });
+        });
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
